6d101ebf8d5a8791b08ec2d7ba20a6970f82bbc3
dylanonelson <dylanonelson@gmail.com>
2016-03-13 17:02:07 -0400

    Put Selenium startup into separate module

--- tasks/macro.js
+++ tasks/macro.js
@@ -8,10 +8,8 @@
 
 'use strict';
 
-var request = require('request');
-var q = require('q');
-var selenium = require('selenium-standalone');
 var configWatcher = require('./configWatcher');
+var macroSelenium = require('./macroSelenium');
 
 module.exports = function(grunt) {
 
@@ -26,53 +24,9 @@ module.exports = function(grunt) {
     macros = configWatcher.getMacros(this.data.macroFile);
     configWatcher.watchMacroFile();
 
-    // Start Selenium server and log output
-    var installSelenium = function() {
-      selenium.install()
-    };
-
-    var startSelenium = function() {
-      console.log('Starting Selenium server...');
-      selenium.start({
-          spawnOptions: {
-            detached: true
-          }
-        }, function(err, child) {
-          if (err != undefined) {
-            console.log('There was an error starting the Selenium server');
-          }
-          child.stderr.on('data', function (data) {
-            console.log(data.toString());
-          });
-      });
-    };
-
-    // Wait for Selenium server startup
-    var checkForSelenium = function() {
-      return q.Promise(function (resolve, reject, notify) {
-        var interval = setInterval(function() {
-          request.get('http://127.0.0.1:4444/wd/hub', function (error, response, body) {
-            if (error != undefined) {
-              return;
-            }
-            if (response != undefined && response.statusCode === 200) {
-              clearInterval(interval);
-              console.log('Connection established!');
-              resolve('http://127.0.0.1:4444/wd/hub');
-            }
-          }).on('error', function (e) {
-            return;
-          })
-        }, 200);
-      });
-    }
-
-    var setupDriver = function (hub) {
-      driver = macros.setup(hub);
-    }
-
-    q(installSelenium).then(startSelenium).then(checkForSelenium).then(setupDriver);
-
+    macroSelenium.start().then(function(hub) {
+        driver = macros.setup(hub);
+    })
   });
 
 };
---
+++ tasks/macroSelenium.js
@@ -0,0 +1,48 @@
+var selenium = require('selenium-standalone');
+var request = require('request');
+var q = require('q');
+
+module.exports.start = function () {
+  // Start Selenium server and log output
+  var installSelenium = function() {
+    selenium.install()
+  };
+
+  var startSelenium = function() {
+    console.log('Starting Selenium server...');
+    selenium.start({
+        spawnOptions: {
+          detached: true
+        }
+      }, function(err, child) {
+        if (err != undefined) {
+          console.log('There was an error starting the Selenium server');
+        }
+        child.stderr.on('data', function (data) {
+          console.log(data.toString());
+        });
+    });
+  };
+
+  // Wait for Selenium server startup
+  var checkForSelenium = function() {
+    return q.Promise(function (resolve, reject, notify) {
+      var interval = setInterval(function() {
+        request.get('http://127.0.0.1:4444/wd/hub', function (error, response, body) {
+          if (error != undefined) {
+            return;
+          }
+          if (response != undefined && response.statusCode === 200) {
+            clearInterval(interval);
+            console.log('Connection established!');
+            resolve('http://127.0.0.1:4444/wd/hub');
+          }
+        }).on('error', function (e) {
+          return;
+        })
+      }, 200);
+    });
+  }
+
+  return q(installSelenium).then(startSelenium).then(checkForSelenium);
+}
